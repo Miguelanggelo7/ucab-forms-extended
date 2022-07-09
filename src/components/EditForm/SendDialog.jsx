54694a42e5783781475be1e85f0f6d78a065a28a
Jose Saad <josemiguelsaad13@gmail.com>
2022-07-09 17:23:01 -0400

    download QR

--- src/components/EditForm/SendDialog.jsx
+++ src/components/EditForm/SendDialog.jsx
@@ -1,4 +1,4 @@
-import { useMemo } from "react";
+import { useMemo, useRef, useCallback } from "react";
 import {
   Button,
   Dialog,
@@ -11,10 +11,17 @@ import { useSnackbar } from "notistack";
 import { useForm } from "../../hooks/useForm";
 import { APP_URL } from "../../constants/urls";
 import QRCode from "react-qr-code";
+import { useCapture } from "react-capture";
 
 const SendDialog = ({ open, setOpen }) => {
   const { form } = useForm();
   const { enqueueSnackbar } = useSnackbar();
+  const { snap } = useCapture();
+  const element = useRef(null);
+
+  const downloadQR = useCallback(() => {
+    snap(element, { file: `${form.title}-QR.png` });
+  }, [snap, element]);
 
   return useMemo(() => {
     const handleClose = () => {
@@ -39,9 +46,11 @@ const SendDialog = ({ open, setOpen }) => {
           <div
             style={{
               margin: "auto",
-              maxWidth: "192.5pt",
+              maxWidth: "192pt",
               width: "100%",
+              maxHeight: "192pt",
             }}
+            ref={element}
           >
             <QRCode value={formUrl} />
           </div>
@@ -57,7 +66,9 @@ const SendDialog = ({ open, setOpen }) => {
             }}
           />
         </DialogContent>
-        <DialogActions>
+        <DialogActions sx={{ marginTop: "-15pt" }}>
+          <Button onClick={downloadQR}>Descargar QR</Button>
+          <div style={{ flex: "1 0 0" }} />
           <Button onClick={handleClose}>Cerrar</Button>
           <Button onClick={handleCopy}>Copiar URL</Button>
         </DialogActions>
