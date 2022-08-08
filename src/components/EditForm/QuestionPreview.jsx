89cc9caed41f26b76fe3326ee21b80de2f6d5d52
Saad <josemiguelsaad13@gmail.com>
2022-08-08 16:38:46 -0500

    swiper to images in question

--- src/components/EditForm/QuestionPreview.jsx
+++ src/components/EditForm/QuestionPreview.jsx
@@ -1,10 +1,29 @@
 import { useMemo } from "react";
-import { Card, IconButton, Tooltip, Typography } from "@mui/material";
+import {
+  Card,
+  IconButton,
+  Tooltip,
+  Typography,
+  Box,
+  Grid,
+} from "@mui/material";
 import { useForm } from "../../hooks/useForm";
 import AllQuestionsPreview from "../AllQuestionsPreview";
 import RequiredMark from "../RequiredMark";
 import AddPhotoAlternateIcon from "@mui/icons-material/AddPhotoAlternate";
 import { motion } from "framer-motion";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+import SwiperCore, { Pagination, Navigation } from "swiper";
+
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import "swiper/css/bundle";
+import "./Slider.css";
+
+SwiperCore.use([Pagination]);
+SwiperCore.use([Navigation]);
 
 const EditQuestion = ({ question, setOpenDrawer }) => {
   const { current, setCurrent } = useForm();
@@ -34,6 +53,33 @@ const EditQuestion = ({ question, setOpenDrawer }) => {
               <Typography variant="caption">{question.description}</Typography>
             </Typography>
           </div>
+          <Swiper
+            slidesPerView={1}
+            pagination={{ clickable: "true" }}
+            className="mySwiper"
+            onSlideChange={() => console.log("slide change")}
+            onSwiper={(swiper) => console.log(swiper)}
+            style={{ marginBottom: "10pt" }}
+          >
+            {question.image?.map((file, i) => (
+              <SwiperSlide>
+                <Grid
+                  container
+                  spacing={0}
+                  direction="column"
+                  alignItems="center"
+                  justifyContent="center"
+                >
+                  <img
+                    alt="imagen"
+                    key={i}
+                    src={file.url}
+                    style={{ maxHeight: "300pt", objectFit: "contain" }}
+                  />
+                </Grid>
+              </SwiperSlide>
+            ))}
+          </Swiper>
           <AllQuestionsPreview question={question} />
         </Card>
       </motion.div>
